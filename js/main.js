4706ce4e87073e483f7060bab1e01f1696ab0312
martin donadieu <martindonadieu@gmail.com>
2016-02-03 00:31:27 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ function init()
 	controls = new THREE.OrbitControls( camera, renderer.domElement );
 	scene.add(new THREE.AxisHelper(100));
 	// scene.fog = new THREE.FogExp2( 0x111111, 0.1 );
-	  scene.fog = new THREE.FogExp2( 0x111111, 0.001 );
+	  scene.fog = new THREE.FogExp2( 0x111111, 0.00001 );
 	//scene.fog = new THREE.FogExp2( 0x333333, 0.00006 );
 }
 
@@ -45,7 +45,7 @@ function deletefdf()
 		scene = new THREE.Scene();
 		scene.add(camera);
 		scene.add(new THREE.AxisHelper(100));
-		 scene.fog = new THREE.FogExp2( 0x111111, 0.001 );
+		 scene.fog = new THREE.FogExp2( 0x111111, 0.00001 );
 		//scene.fog = new THREE.FogExp2( 0x333333, 0.00007 );
 	//	scene.fog = new THREE.FogExp2( 0x9999ff, 0.00000001 );
 	}
